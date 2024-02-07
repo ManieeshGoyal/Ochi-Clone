6cdcb7273d6b62b519361fe070371544c6e92214
Manish Goyal <manieeshgoyal@gmail.com>
2024-02-07 17:31:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Navbar from "./components/navbar";
+("./components/navbar.jsx");
+
+export default function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-900 text-white">
+      <Navbar />
+    </div>
+  );
+}
